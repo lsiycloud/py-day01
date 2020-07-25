cdae339669030e825f42bb148964b3aad5b9f13e
siy <lsydeem183@163.com>
2020-07-25 21:30:39 +0800

    check_ip

---
+++ a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#check ip up or down
+echo "Please enter a ip : (*.*.*.*/*) "
+read x
+ping -c 2 -w 1 $x &> /dev/null
+if [ $? -eq 0 ];then
+  echo "$x" is up
+else
+  echo "$x" is down
+fi
